399db74bec62b99e65d779f1a4c5e3987c388e23
Şevket Ay <63016752+sevketay09@users.noreply.github.com>
2023-09-05 11:12:22 +0300

    Add files via upload

---
+++ create_dataset.py
@@ -0,0 +1,71 @@
+'''
+Bu komut dosyasi, özellik görünümlerinden 
+özellikleri getirecek, 
+bunlara katilacak ve bunlari özellik mağazamizda bir veri kümesi olarak kaydedecektir.
+'''
+# Importing dependencies
+import pandas as pd
+from feast import FeatureStore
+from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
+
+# Getting our FeatureStore
+store = FeatureStore(repo_path="breast_cancer/")
+
+# Reading our targets as an entity DataFrame
+entity_df = pd.read_parquet(path="breast_cancer/data/target_df.parquet")    
+
+'''
+Feast, farkli özellik görünümlerini bir araya getirmek için entity DataFrame'lerini kullanir. 
+Zamanin doğruluğunu sağlamak için Feast, 
+feature viewdeki ve entity DataFrame'deki 
+entity ve event timestampleri eşleştirir.
+
+'''
+training_data = store.get_historical_features(
+    entity_df=entity_df,
+    features=[
+        "df1_feature_view:mean radius",
+        "df1_feature_view:mean texture",
+        "df1_feature_view:mean perimeter",
+        "df1_feature_view:mean area",
+        "df1_feature_view:mean smoothness",
+        "df2_feature_view:mean compactness",
+        "df2_feature_view:mean concavity",
+        "df2_feature_view:mean concave points",
+        "df2_feature_view:mean symmetry",
+        "df2_feature_view:mean fractal dimension",
+        "df3_feature_view:radius error",
+        "df3_feature_view:texture error",
+        "df3_feature_view:perimeter error",
+        "df3_feature_view:area error",
+        "df3_feature_view:smoothness error",
+        "df3_feature_view:compactness error",
+        "df3_feature_view:concavity error",
+        "df4_feature_view:concave points error",
+        "df4_feature_view:symmetry error",
+        "df4_feature_view:fractal dimension error",
+        "df4_feature_view:worst radius",
+        "df4_feature_view:worst texture",
+        "df4_feature_view:worst perimeter",
+        "df4_feature_view:worst area",
+        "df4_feature_view:worst smoothness",
+        "df4_feature_view:worst compactness",
+        "df4_feature_view:worst concavity",
+        "df4_feature_view:worst concave points",
+        "df4_feature_view:worst symmetry",
+        "df4_feature_view:worst fractal dimension"
+    ]
+)
+
+
+'''
+get_historical_features yöntemi, etiketlerle birlikte entity_df'mizi alır, onu parametre özelliklerine iletilen özelliklerle birleştirir 
+ve verilere tanıtıcı görevi gören bir RetrievalJob nesnesi döndürür
+'''
+
+# Storing the dataset as a local file
+dataset = store.create_saved_dataset(
+    from_=training_data,
+    name="breast_cancer_dataset",
+    storage=SavedDatasetFileStorage("breast_cancer/data/breast_cancer_dataset.parquet")
+)
---
+++ materialize.py
@@ -0,0 +1,19 @@
+# Importing dependencies
+from feast import FeatureStore
+from datetime import datetime, timedelta
+
+# Getting our FeatureStore
+store = FeatureStore(repo_path="breast_cancer/")
+
+# Code for loading features to online store between two dates
+"""store.materialize(
+    end_date=datetime.now(),
+    start_date=datetime.now() - timedelta(days=700))"""
+
+# Loading the latest features after a previous materialize call or from the beginning of time
+store.materialize_incremental(end_date=datetime.now())
+
+'''
+Kodu çalıştırdıktan sonra Feast, 
+en son özellik değerlerini getirecek ve bunları breast_cancer/data/online_store.db'de saklayacaktır.
+'''
